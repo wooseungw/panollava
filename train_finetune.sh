b0a2a4216a24c84f8f8b86f1f73b7686cbd1f734
wooseungw <woow070840@gmail.com>
2025-07-16 19:15:00 +0900

    feat: 학습 스크립트 추가 및 환경 변수 설정

--- train_finetune.sh
+++ train_finetune.sh
@@ -1,4 +1,5 @@
-
+export CUDA_VISIBLE_DEVICES=1
+export WANDB_API_KEY=9fd21364ed6c1c6677a250972c5e19a931171974
 #!/bin/bash
 # Finetune 스테이지 학습
-python train.py --stage finetune --csv-train data/quic360/train.csv --csv-val data/quic360/valid.csv --vision-name google/siglip-base-patch16-224 --lm-name Qwen/Qwen3-0.6B --resampler mlp --epochs 1 --batch-size 16 --lr 2e-6 --max-txt-len 128 --wandb-project panorama-vlm
+python train.py --stage finetune --csv-train data/quic360/train.csv --csv-val data/quic360/valid.csv --vision-name google/siglip-base-patch16-224 --lm-name Qwen/Qwen3-0.6B --resampler mlp --epochs 1 --batch-size 16 --lr 2e-6 --max-txt-len 32 --wandb-project panorama-vlm
--- train_single_stage.sh
+++ train_single_stage.sh
@@ -1,3 +1,5 @@
+export CUDA_VISIBLE_DEVICES=1
+export WANDB_API_KEY=9fd21364ed6c1c6677a250972c5e19a931171974
 #!/bin/bash
 # 단일 스테이지 학습 (예: vision)
 python train.py --stage vision --csv-train data/quic360/train.csv --csv-val data/quic360/valid.csv --vision-name google/siglip-base-patch16-224 --lm-name Qwen/Qwen3-0.6B --resampler mlp --epochs 1 --batch-size 4 --lr 5e-5 --max-txt-len 128 --wandb-project panorama-vlm
